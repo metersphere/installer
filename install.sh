32c926eddbf23c4f5a69b63eeebea4d570a742fa
王振 <wangzhen@fit2cloud.com>
2020-05-19 11:23:59 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+BASE_DIR=$(cd "$(dirname "$0")";pwd)
+source ${BASE_DIR}/install_config.sh
+action=$1
+target=$2
+args=$@
+
+compose_files="-f docker-compose-base.yml"
+
+echo "== 开始安装 MeterSphere =="
+echo "... 拷贝资源至安装路径"
+cp -r ./metersphere $(install_config base_dir) && echo "... 拷贝资源至安装路径成功"
+echo "... 替换安装配置参数"
+for i in ${support_config};do
+   key=${i}
+   value=$(install_config $i)
+   if [[ -n $key && -n $value ]];then
+      sed -i -e "s#\${${key}}#${value}#g" $(install_config base_dir)/metersphere/conf/* $(install_config base_dir)/metersphere/bin/mysql/* $(install_config base_dir)/metersphere/docker-compose*
+   fi
+done
+echo "... 配置安装模式"
+install_mode=$(install_config install_mode)
+case ${install_mode} in
+   allinone)
+      mkdir -p $(install_config base_dir)/metersphere/data/jmeter
+      compose_files="${compose_files} -f docker-compose-server.yml -f docker-compose-node-controller.yml"
+      ;;
+   server)
+      compose_files="${compose_files} -f docker-compose-server.yml" 
+      ;;
+   node-controller)
+      mkdir -p $(install_config base_dir)/metersphere/data/jmeter
+      compose_files="${compose_files} -f docker-compose-node-controller.yml" 
+      ;;
+   *)
+      echo "... 不支持的安装模式，请从 [ allinone | server | node-controller ] 中进行选择"
+esac
+# 是否使用外部数据库
+if [ $(install_config external_mysql) = "false" ];then
+   mkdir -p $(install_config base_dir)/metersphere/data/mysql
+   compose_files="${compose_files} -f docker-compose-mysql.yml"
+else
+   sed -i -e "/#external_mysql=false/{N;N;d;}" $(install_config base_dir)/metersphere/docker-compose*
+fi
+# 是否使用外部 Kafka
+if [ $(install_config external_kafka) = "false" ];then
+   mkdir -p $(install_config base_dir)/metersphere/data/kafka
+   mkdir -p $(install_config base_dir)/metersphere/data/zookeeper
+   compose_files="${compose_files} -f docker-compose-kafka.yml" 
+else
+   sed -i -e "/#external_kafka=false/{N;N;d;}" $(install_config base_dir)/metersphere/docker-compose*
+fi
+# TODO 加载镜像
+
+echo "... 启动 MeterSphere"
+echo ${compose_files} > $(install_config base_dir)/metersphere/compose_files 
+cd $(install_config base_dir)/metersphere && docker-compose $(cat compose_files) up -d
--- msctl.sh
+++ msctl.sh
@@ -1,6 +1,7 @@
+# TODO
+## 增加单独的 install.sh 脚本
+## 安装完成后拷贝 msctl 到 bin 目录, 作为或许升级、重启等命令控制脚本
 #!/bin/bash
-BASE_DIR=$(cd "$(dirname "$0")";pwd)
-source ${BASE_DIR}/install_config.sh
 action=$1
 target=$2
 args=$@
@@ -9,75 +10,59 @@ compose_files="-f docker-compose-base.yml"
 #images = 
 
 function usage() {
-   echo "MeterSphere 部署安装脚本"
+   echo "MeterSphere 控制脚本"
    echo
    echo "Usage: "
    echo "  ./msctl.sh [COMMAND] [ARGS...]"
    echo "  ./msctl.sh --help"
    echo
    echo "Commands: "
-   echo "  install 部署安装 MeterSphere"
-}
-
-function install() {
-   echo "== 开始安装 MeterSphere =="
-   echo "... 拷贝资源至安装路径"
-   cp -r ./metersphere $(install_config base_dir) && echo "... 拷贝资源至安装路径成功"
-   echo "... 替换安装配置参数"
-   for i in ${support_config};do
-      key=${i}
-      value=$(install_config $i)
-      if [[ -n $key && -n $value ]];then
-         sed -i -e "s#\${${key}}#${value}#g" $(install_config base_dir)/metersphere/conf/* $(install_config base_dir)/metersphere/bin/mysql/* $(install_config base_dir)/metersphere/docker-compose*
-      fi
-   done
-   echo "... 配置安装模式"
-   install_mode=$(install_config install_mode)
-   case ${install_mode} in
-      allinone)
-         mkdir -p $(install_config base_dir)/metersphere/data/jmeter
-         compose_files="${compose_files} -f docker-compose-server.yml -f docker-compose-node-controller.yml"
-         ;;
-      server)
-         compose_files="${compose_files} -f docker-compose-server.yml" 
-         ;;
-      node-controller)
-         mkdir -p $(install_config base_dir)/metersphere/data/jmeter
-         compose_files="${compose_files} -f docker-compose-node-controller.yml" 
-         ;;
-      *)
-         echo "... 不支持的安装模式，请从 [ allinone | server | node-controller ] 中进行选择"
-   esac
-   # 是否使用外部数据库
-   if [ $(install_config external_mysql) = "false" ];then
-      mkdir -p $(install_config base_dir)/metersphere/data/mysql
-      compose_files="${compose_files} -f docker-compose-mysql.yml"
-   else
-      sed -i -e "/#external_mysql=false/{N;N;d;}" $(install_config base_dir)/metersphere/docker-compose*
-   fi
-   # 是否使用外部 Kafka
-   if [ $(install_config external_kafka) = "false" ];then
-      mkdir -p $(install_config base_dir)/metersphere/data/kafka
-      mkdir -p $(install_config base_dir)/metersphere/data/zookeeper
-      compose_files="${compose_files} -f docker-compose-kafka.yml" 
-   else
-      sed -i -e "/#external_kafka=false/{N;N;d;}" $(install_config base_dir)/metersphere/docker-compose*
-   fi
-   # TODO 加载镜像
-
-   echo "... 启动 MeterSphere"
-   echo ${compose_files}
-   cd $(install_config base_dir)/metersphere && docker-compose ${compose_files} up -d
+   echo "  status    查看 MeterSphere 服务运行状态"
+   echo "  start     启动 MeterSphere 服务运行状态"
+   echo "  stop      停止 MeterSphere 服务"
+   echo "  restart   重启 MeterSphere 服务"
+   echo "  uninstall 卸载 MeterSphere 服务运行状态"
+   echo "  version   查看 MeterSphere 版本信息"
 }
 
 function status() {
    echo
 }
+function start() {
+   echo
+}
+function stop() {
+   echo
+}
+function restart() {
+   echo
+}
+function uninstall() {
+   echo
+}
+function version() {
+   echo
+}
 
 function main() {
     case "${action}" in
-      install)
-         install
+      status)
+         status
+         ;;
+      start)
+         start
+         ;;
+      stop)
+         stop
+         ;;
+      restart)
+         restart
+         ;;
+      uninstall)
+         uninstall
+         ;;
+      version)
+         version
          ;;
       help)
          usage
