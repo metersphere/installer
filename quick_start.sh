0a6a67431176544db9732a6c321dc71410ce86ae
王振 <wangzhen@fit2cloud.com>
2020-05-19 10:38:36 +0800

    update quick_start.sh

--- quick_start.sh
+++ quick_start.sh
@@ -1,15 +1,11 @@
-
-
-#Harbor on Ubuntu 18.04
-
-#安装 docker & docker-compose
-#Install Latest Stable Docker Release
+#Install docker & docker-compose
+##Install Latest Stable Docker Release
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 service docker start
 echo "Docker Installation done"
 
-#Install Latest Stable Docker Compose Release
+##Install Latest Stable Docker Compose Release
 COMPOSEVERSION=$(curl -s https://github.com/docker/compose/releases/latest/download 2>&1 | grep -Po [0-9]+\.[0-9]+\.[0-9]+)
 curl -L "https://github.com/docker/compose/releases/download/$COMPOSEVERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -21,8 +17,6 @@ echo "Docker Compose Installation done"
 #curl -s https://api.github.com/repos/metersphere/metersphere/releases/latest | grep browser_download_url | grep online | cut -d '"' -f 4 | wget -qi -
 tar zxvf metersphere-release-${MSVERSION}.tar.gz
 cd metersphere-release-${MSVERSION}
+echo "metersphere_image_tag=${MSVERSION}" > install.conf
 ./msctl.sh install
-cat <<EOF > install.conf
-metersphere_image_tag=v0.1
-EOF
 echo -e "MeterSphere Installation Complete \n\nLogin to your MeterSphere instance:\n URL: http://$LOCAL_IP:8081\n Username: admin Password: fit2cloud"
