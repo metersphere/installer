947c28764edd2a85e27dfe097208527d7d433fc7
王振 <wangzhen@fit2cloud.com>
2020-06-01 15:38:37 +0800

    更新快速开始脚本

--- quick_start.sh
+++ quick_start.sh
@@ -21,13 +21,14 @@ else
 fi
 
 #Install Latest Stable MeterSphere Release
-#MSVERSION=$(curl -s https://github.com/metersphere/metersphere/releases/latest/download 2>&1 | grep -Po v[0-9]+\.[0-9]+\.[0-9]+)
+MSVERSION=$(curl -s https://github.com/metersphere/metersphere/releases/latest/download 2>&1 | grep -Po v[0-9]+\.[0-9]+\.[0-9]+)
+wget https://github.com/metersphere/metersphere/releases/latest/download/metersphere-release-${MSVERSION}.tar.gz
 #curl -s https://api.github.com/repos/metersphere/metersphere/releases/latest | grep browser_download_url | grep online | cut -d '"' -f 4 | wget -qi -
 tar zxvf metersphere-release-${MSVERSION}.tar.gz
 cd metersphere-release-${MSVERSION}
-echo "MS_TAG=${MSVERSION}" > install.conf
-echo "MS_PREFIX=metersphere" >> install.conf
+sed -i -e "s\MS_TAG=.*\MS_TAG=${MSVERSION}\g" install.conf
+sed -i -e "s\MS_PREFIX=.*\MS_PREFIX=registry.cn-qingdao.aliyuncs.com/metersphere\g" install.conf
 sh install.sh
 msctl status
-echo -e "MeterSphere Installation Complete \n\nLogin to your MeterSphere instance:\n URL: http://$LOCAL_IP:8081\n Username: admin Password: fit2cloud"
+echo -e "MeterSphere Installation Complete \n\nLogin to your MeterSphere instance:\n URL: http://$LOCAL_IP:8081\n Username: admin Password: metersphere"
 echo -e "You can use command 'msctl status' to check the status of MeterSphere."
