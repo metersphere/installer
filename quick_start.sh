484afd382e4a8b843351ff5959dd6c89ecc08855
王振 <wangzhen@fit2cloud.com>
2020-06-08 11:25:06 +0800

    update grep expression

--- quick_start.sh
+++ quick_start.sh
@@ -1,5 +1,5 @@
 #Install Latest Stable MeterSphere Release
-MSVERSION=$(curl -s https://github.com/metersphere/metersphere/releases/latest/download 2>&1 | grep -Po v[0-9]+\.[0-9]+\.[0-9]+)
+MSVERSION=$(curl -s https://github.com/metersphere/metersphere/releases/latest/download 2>&1 | grep -Po 'v[0-9]+\.[0-9]+\.[0-9]+.*(?=")'
 wget https://github.com/metersphere/metersphere/releases/latest/download/metersphere-release-${MSVERSION}.tar.gz
 #curl -s https://api.github.com/repos/metersphere/metersphere/releases/latest | grep browser_download_url | grep online | cut -d '"' -f 4 | wget -qi -
 tar zxvf metersphere-release-${MSVERSION}.tar.gz
