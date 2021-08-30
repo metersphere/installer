770c85cc875c8a6a940564105b3b2dbbad0db936
BugKing <wangzhen@fit2cloud.com>
2021-08-30 14:23:50 +0800

    refactor: 修改脚本中使用的安装包规则

--- quick_start.sh
+++ quick_start.sh
@@ -9,10 +9,10 @@ else
 	MSVERSION=$(curl -s https://github.com/metersphere/metersphere/releases/latest/download 2>&1 | grep -Po 'v[0-9]+\.[0-9]+\.[0-9]+.*(?=")')
 fi
 
-wget --no-check-certificate https://github.com/metersphere/metersphere/releases/latest/download/metersphere-release-${MSVERSION}.tar.gz
+wget --no-check-certificate https://github.com/metersphere/metersphere/releases/latest/download/metersphere-online-installer-${MSVERSION}.tar.gz
 #curl -s https://api.github.com/repos/metersphere/metersphere/releases/latest | grep browser_download_url | grep online | cut -d '"' -f 4 | wget -qi -
-tar zxvf metersphere-release-${MSVERSION}.tar.gz
-cd metersphere-release-${MSVERSION}
+tar zxvf metersphere-online-installer-${MSVERSION}.tar.gz
+cd metersphere-online-installer-${MSVERSION}
 
 sed -i -e "s#MS_IMAGE_TAG=.*#MS_IMAGE_TAG=${MSVERSION}#g" install.conf
 sed -i -e "s#MS_IMAGE_PREFIX=.*#MS_IMAGE_PREFIX=registry.cn-qingdao.aliyuncs.com\/metersphere#g" install.conf
